6cbf771fd3316d8ce5a02d6833a48d90c1253496
NOTE-CLAU\claudinei.goncalves <claudineicg@gmail.com>
2018-05-09 21:18:04 -0300

    Implementação RrApi

---
+++ Framework.AddApp.Mobile.Api/ApiModels/RrRequest.cs
@@ -0,0 +1,13 @@
+﻿using Framework.AddApp.Mobile.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework.AddApp.Mobile.ApiModels
+{
+    public class RrRequest : BaseRequest
+    {
+        public int Registro { get; set; }
+        public int Item { get; set; }
+    }
+}
---
+++ Framework.AddApp.Mobile.Api/ApiModels/RrResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework.AddApp.Mobile.ApiModels
+{
+    public class RrResponse
+    {
+        public String Cliente { get; set; }
+        public String Equipamento { get; set; }
+        public int Nota { get; set; }
+        public String Data { get; set; }
+    }
+}
